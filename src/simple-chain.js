52b4bda409014497cfe1b21f5e8e7ebeeaac24a0
Alexander Naiden <naidenav@gmail.com>
2021-03-01 18:46:01 +0300

    feat: complete simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,24 +2,57 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.chain) {
+      return 0;
+    } else {
+      return this.chain.length;
+    }
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.chain) {
+      if (value === undefined) {
+        this.chain = [''];
+        return this;
+      } else {
+        this.chain = [`${value}`];
+        return this;
+      }
+    } else {
+      if (value === undefined) {
+        this.chain.push('');
+        return this;
+      } else {
+        this.chain.push(`${value}`);
+        return this;
+      }
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.chain[position - 1] || typeof(position) != 'number' || !Number.isInteger(position)) {
+      delete this.chain;
+      throw new Error('Ups');
+    } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain) {
+      this.chain.reverse();
+      return this;
+    } else {
+      return this;
+    }
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain) {
+      let chainString = `( ${this.chain[0]} )`;
+      for (let i = 1; i < this.chain.length; i++) {
+        chainString += `~~( ${this.chain[i]} )`;
+      }
+      delete this.chain;
+      return chainString;
+    }
   }
 };
 
