8f281d72ef09d0a0c691661363ca6b2fc0323fc5
Alexander Naiden <naidenav@gmail.com>
2021-03-03 19:35:12 +0300

    feat: finish vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
   }
+  encrypt(string, keyword) {
+	string = string.toLowerCase();
+	keyword = keyword.toLowerCase();
+	let lengthOfKeyword = keyword.length;
+	if (keyword.length < string.length) {
+		for (let n = 0; n < string.length - lengthOfKeyword; n++) {	
+			keyword += keyword[n];
+		}
+	} else if (keyword.length > string.length) {
+		keyword = keyword.slice(0,string.length);
+	}	 
+	let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	let result = '';
+	for (let i = 0; i < string.length; i++) {
+		if (string[i].toLowerCase() == string[i].toUpperCase()) {
+			result += string[i];
+			keyword = ' ' + keyword;
+ 		} else {
+			let indexOfStringLetter = alphabet.indexOf(string[i]);
+			let indexOfKeywordLetter = alphabet.indexOf(keyword[i]);
+			if (indexOfKeywordLetter + indexOfStringLetter < 26) { 
+				result += alphabet[indexOfKeywordLetter + indexOfStringLetter];
+			} else {
+				result += alphabet[indexOfKeywordLetter - 26 + indexOfStringLetter];
+			}
+		}
+	}
+	if (this.type == undefined || this.type === true) {
+    return result.toUpperCase();	
+  } else {
+    return result.toUpperCase().split('').reverse().join('');	
+  }
+  }    
+  decrypt(string, keyword) {
+	string = string.toLowerCase();
+	keyword = keyword.toLowerCase();
+	let lengthOfKeyword = keyword.length;
+	if (keyword.length < string.length) {
+		for (let n = 0; n < string.length - lengthOfKeyword; n++) {
+				keyword += keyword[n];
+		}
+	} else if (keyword.length > string.length) {
+		keyword = keyword.slice(0,string.length);
+	}	 
+	let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	let result = '';
+	for (let i = 0; i < string.length; i++) {
+		if (string[i].toLowerCase() == string[i].toUpperCase()) {
+			result += string[i];
+			keyword = ' ' + keyword;
+		} else {
+			let indexOfStringLetter = alphabet.indexOf(string[i]);
+			let indexOfKeywordLetter = alphabet.indexOf(keyword[i]);
+			if (indexOfStringLetter >= indexOfKeywordLetter) { 
+				result += alphabet[indexOfStringLetter - indexOfKeywordLetter];
+			} else {
+				result += alphabet[indexOfStringLetter + 26 - indexOfKeywordLetter];
+			}
+		}
+	}
+  if (this.type ==undefined || this.type === true) {
+    return result.toUpperCase();	
+  } else {
+    return result.toUpperCase().split('').reverse().join('');	
+  }
+}
 }
 
 module.exports = VigenereCipheringMachine;
