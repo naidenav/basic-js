159f51f16fc96622e11cd8f8146b4f81290c6bb7
Alexander Naiden <naidenav@gmail.com>
2021-03-03 20:24:35 +0300

    fix: update repository

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,6 +6,6 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
 	if (!sampleActivity  || sampleActivity <= 0 || sampleActivity >= 15 || typeof(sampleActivity) != 'string' || !(sampleActivity / 2)) {return false};
 	const rateConstant = 0.693 / HALF_LIFE_PERIOD;
-	let dateSample = MODERN_ACTIVITY / sampleActivity / rateConstant;
+	let dateSample = Math.log(MODERN_ACTIVITY / sampleActivity) / rateConstant;
 	return Math.ceil(dateSample);
 };
